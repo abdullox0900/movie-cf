8d4030c12f2440d03cf3f4fd947e1c96ed5f6f77
abdullox0900 <abdullox0900@gmail.com>
2022-01-25 06:54:37 +0500

    add js modal

--- js/script.js
+++ js/script.js
@@ -1,13 +1,42 @@
 "use stric"
-
 let elForm = document.querySelector(".form");
-let elInput = document.querySelector(".form__input");
+let elInput = document.querySelector(".form__input")
 let elList = document.querySelector(".movie__list");
+let elSelect = document.querySelector(".form__select");
+let elFilmInfo = document.querySelector(".film-info");
+
+
+const generateGenres = function (films) {
+     const filteredGenres = [];
+
+     films.forEach(film => {
+          film.genres.forEach(genre => {
+               if (!filteredGenres.includes(genre)) {
+                    filteredGenres.push(genre)
+               }
+          });
+     });
+
+     filteredGenres.forEach(genre => {
+          let newOption = document.createElement("option");
+
+          newOption.value = genre;
+          newOption.textContent = genre;
+
+          elSelect.appendChild(newOption);
+     });
+};
+
+generateGenres(films);
+
 
 
 const filmsRendom = function (filmsArr, element) {
+
      filmsArr.forEach(film => {
           //MOVIE CARD
+
+          let elBody = document.body;
           let elItem = document.createElement("li");
           let elMovieCrard = document.createElement("div");
           // Movie-Card-Header
@@ -19,10 +48,19 @@ const filmsRendom = function (filmsArr, element) {
           let movieOverlayReting = document.createElement("div");
           let movieRetingInfo = document.createElement("span");
           let movieOverlayPlayIcon = document.createElement("div");
+          let movieOverlayInfo = document.createElement("div");
           // Movie-Card-Body
           let movieCardBody = document.createElement("div");
           let movieCardBodyTitle = document.createElement("h3");
           let movieCardBodyGenres = document.createElement("ul");
+          // Movie-Modal
+          let movieModal = document.createElement("div");
+          let movieModalOverlay = document.createElement("div");
+          let movieModalPanel = document.createElement("div");
+          let movieModalPanelBtn = document.createElement("button");
+          let movieModalImg = document.createElement("img");
+          let movieModalTitle = document.createElement("h2");
+          let movieModalPanleText = document.createElement("p");
 
 
           //SETATTRIBUTE
@@ -38,15 +76,26 @@ const filmsRendom = function (filmsArr, element) {
           movieOverlayReting.setAttribute("class", "rating");
           movieRetingInfo.setAttribute("class", "movie__rating");
           movieOverlayPlayIcon.setAttribute("class", "play");
+          movieOverlayInfo.setAttribute("class", "film-info")
           // Movie-Card-Body
           movieCardBody.setAttribute("class", "movie__card-body");
           movieCardBodyTitle.setAttribute("class", "movie__card-title");
           movieCardBodyGenres.setAttribute("class", "movie__card-info");
-
+          // Movie-Modal
+          movieModal.setAttribute("class", "movie__over");
+          movieModalOverlay.setAttribute("class", "overlay");
+          movieModalPanel.setAttribute("class", "movie__panel");
+          movieModalPanelBtn.setAttribute("class", "movie__panel-btn");
+          movieModalImg.setAttribute("class", "movie__panel-img");
+          movieModalImg.setAttribute("src", film.poster);
+          movieModalTitle.setAttribute("class", "movie-header");
+          movieModalPanleText.setAttribute("class", "movie-info");
 
           // TEXTCONTENT
           movieRetingInfo.textContent = film.reting;
           movieCardBodyTitle.textContent = film.title.slice(0, 20);
+          movieModalTitle.textContent = film.title;
+          movieModalPanleText.textContent = film.overview.slice(0, 500);
 
           //APPENDCHILD
           element.appendChild(elItem);
@@ -60,17 +109,78 @@ const filmsRendom = function (filmsArr, element) {
           movieOverlay.appendChild(movieOverlayReting);
           movieOverlayReting.appendChild(movieRetingInfo);
           movieOverlay.appendChild(movieOverlayPlayIcon);
+          movieOverlay.appendChild(movieOverlayInfo);
           // Movie-Card-Body
           elMovieCrard.appendChild(movieCardBody);
           movieCardBody.appendChild(movieCardBodyTitle);
           movieCardBody.appendChild(movieCardBodyGenres);
+          // Movie-Modal
+          elBody.appendChild(movieModal);
+          movieModal.appendChild(movieModalPanel);
+          movieModalPanel.appendChild(movieModalPanelBtn);
+          movieModalPanel.appendChild(movieModalImg);
+          movieModalPanel.appendChild(movieModalTitle);
+          movieModal.appendChild(movieModalOverlay);
+          movieModalPanel.appendChild(movieModalPanleText);
 
-          // film.genres.forEach(genre => {
-          //      let elGenresItem = document.createElement("li");
-          //      elGenresItem.textContent = genre
-          //      movieCardBodyGenres.appendChild(elGenresItem);
-          // })
+          movieModal.classList.add("hidden");
+          movieModalOverlay.classList.add("hidden");
+
+          movieOverlayInfo.addEventListener("click", function () {
+               movieModal.classList.remove("hidden");
+               movieModalOverlay.classList.remove("hidden");
+          })
+
+          movieModalPanelBtn.addEventListener("click", function () {
+               movieModal.classList.add("hidden");
+               movieModalOverlay.classList.add("hidden");
+          })
+
+          movieModalOverlay.addEventListener("click", function () {
+               movieModal.classList.add("hidden");
+               movieModalOverlay.classList.add("hidden");
+          })
+
+          // document.addEventListener('keydown', function (evt) {
+          //      evt.preventDefault()
+          //      if (evt.key == "Escape") {
+          //           movieModal.classList.add("hidden");
+          //           movieModalOverlay.classList.add("hidden");
+          //      }
+          // });
      });
 };
 
+
+
+
 filmsRendom(films, elList);
+
+elForm.addEventListener("submit", function (evt) {
+     evt.preventDefault();
+
+     elList.innerHTML = null;
+
+     let selectValue = elSelect.value;
+
+     let filteredFilms = []
+
+     films.forEach(film => {
+          if (selectValue === "all" || film.genres.includes(selectValue)) {
+               filteredFilms.push(film)
+          }
+     })
+
+     filmsRendom(filteredFilms, elList);
+})
+
+elInput.addEventListener('input', (e) => {
+     const inputSearch = e.target.value;
+
+     let filterFilms = films.filter(film =>
+          inputSearch.toLowerCase() === film.title.slice(0, inputSearch.length).toLowerCase(),
+          elList.innerHTML = null
+     )
+     filmsRendom(filterFilms, elList, films);
+     generateGenres(filterFilms);
+})
